8ed8eb8ce136d0e0e9a97301b6c11143557d532c
nicholascannon <nicholascannon1@gmail.com>
2025-05-24 17:48:18 +1000

    rename app.tsx

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import { App } from './App';
+import { App } from './app';
 import './global.css';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
